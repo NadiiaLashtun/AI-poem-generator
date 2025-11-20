0d3b82c9a6b45416ea47208149f8663e6a6745bf
Nadiia Lashtun <flor.smile11@gmail.com>
2025-11-20 17:10:53 +0100

    Add api to generate poem and fix some style

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-function poemGenerate(event) {
-  event.preventDefault();
+function poemDisplay(response) {
+  console.log(response);
 
   new Typewriter('#poem-box', {
-    strings: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    strings: response.data.answer,
     autoStart: true,
     delay: 20,
     cursor: '',
   });
+}
+
+function poemGenerate(event) {
+  event.preventDefault();
+  let topicElement = document.querySelector('#topic');
+  let apiKey = 'b8f1o699a3b107e215cf47a31fc0ate8';
+  let prompt = `Generate poem on topic ${topicElement.value}`;
+  let context =
+    "Your are the best poem writer. Write short poem in 4 lines using topic. and show in HTML format without word html. Sign it like 'SheCodes AI' after poem ot the RIGHT bottom corner in tag <strong>";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(poemDisplay);
 }
 
 let formElement = document.querySelector('#form-poem-generator');
