6b748b729877f1f9a5602faaf2abf55de90ac3b3
Nadiia Lashtun <flor.smile11@gmail.com>
2025-11-20 15:53:36 +0100

    Add handling form

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function poemGenerate(event) {
+  event.preventDefault();
 
+  new Typewriter('#poem-box', {
+    strings: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    autoStart: true,
+    delay: 20,
+    cursor: '',
+  });
+}
+
+let formElement = document.querySelector('#form-poem-generator');
+formElement.addEventListener('submit', poemGenerate);
